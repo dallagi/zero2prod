9059980ad07ed6608f33e230f18569abe31d248f
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-11-15 09:10:22 +0100

    Improve configuration handling for multiple envs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 use sqlx::PgPool;
-use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".to_string(), "info".to_string(), std::io::stdout);
     init_subscriber(subscriber);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
+    let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
