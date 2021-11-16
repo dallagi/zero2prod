fb851598145834dbeccf6873c712b23499ba0e8c
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-11-16 09:15:41 +0100

    Eagerly connect to postgres

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
+use std::time::Duration;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -10,9 +11,12 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(Duration::from_secs(2))
+        .connect(&configuration.database.connection_string())
         .await
-        .expect("Failed to connect to Postgres.");
+        .expect("Failed to connect to Postgres");
 
     let address = format!(
         "{}:{}",
